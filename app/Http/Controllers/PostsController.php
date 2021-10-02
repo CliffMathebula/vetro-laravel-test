b12ffd24f3a49e3c82b9f1cb5aabedd8f614ecbd
cliff mathebula <climathe1@gmail.com>
2021-10-02 05:21:26 -0700

    feat: added code to retieve all posts, and rate posts

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+
+class PostsController extends Controller
+{
+    /**
+     * Show posts dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        return view('posts');
+    }
+
+    /**
+     * Show all posts.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function posts()
+    {
+        $posts = Post::all();
+        return view('posts', compact('posts'));
+    }
+
+    /**
+     * Show single post by user_id.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show($id)
+
+    {
+        $post = Post::find($id);
+        return view('postsShow', compact('post'));
+    }
+
+    /**
+     * rate post.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function postPost(Request $request)
+    {
+        request()->validate(['rate' => 'required']);
+        
+        $post = Post::find($request->id); 
+        $rating = new \willvincent\Rateable\Rating;
+        $rating->rating = $request->rate;
+        $rating->user_id = auth()->user()->id;
+        $post->ratings()->save($rating);
+
+        return redirect()->route("posts");
+    }
+}
