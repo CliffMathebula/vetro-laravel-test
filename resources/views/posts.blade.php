0486fcfb080bce10354b0e3328d4c06600b7039b
cliff mathebula <climathe1@gmail.com>
2021-10-03 01:14:47 -0700

    refactor: added and modified the link to delete post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,38 +7,35 @@
     @endauth
 </div>
 
-<div class="bg-light p-5 rounded mt-2">
+<div class="bg-warning p-5 rounded mt-2">
     @auth
     <h1>Posts</h1>
     <p class="lead"><strong>Please rate the posts below to help us understand th perfect post.</strong></p>
-    <table class="table table-bordered">
-        <tr>
-            <th><h2>Id</h2></th>
-            <th><h2>Name</h2></th>
-            <th><h2>Title</h2></th>
-            <th><h2>content</h2></th>
-            <th width="400px"><h2>Star</h2></th>
-            <th width="100px"><h2>View</h2></th>
-        </tr>
-        @if($posts->count())
-        @foreach($posts as $post)
-        <tr>
-            <td><h4>{{ $post->id }}</h4></td>
-            <td><h4>{{ $post->name }}</h4></td>
-            <td><h4>{{ $post->title }}</h4></td>
-            <td><h4>{{ $post->content }}</h4></td>
-       
-       
-            <td>
-                <input id="input-1" name="input-1" class="rating rating-loading" data-min="0" data-max="5" data-step="0.1" value="{{ $post->averageRating }}" data-size="xs" disabled="">
-            </td>
-            <td>
-                <a href="{{ route('posts.show',$post->id) }}" class="btn btn-primary btn-lg">View</a>
-            </td>
-        </tr>
-        @endforeach
-        @endif
-    </table>
+    <hr />
+    @if($posts->count())
+    @foreach($posts as $post)
+
+    <p><strong>Date Posted: {{ $post->created_at }}</strong></p>
+    <p><strong>Post Name:{{ $post->name }}</strong></p>
+    <p><strong>Post Title: {{ $post->title }}</strong></p>
+    <br />
+    <p><strong> Post Content:</strong></p>
+    <div>
+        <p class="lead">{{ $post->content }}</p>
+    </div>
+
+    <p class="lead"><strong> <input id="input-1" name="input-1" class="rating rating-loading" data-min="0" data-max="5" data-step="0.1" value="{{ $post->averageRating }}" data-size="xs" disabled="">
+            <a href="{{ route('posts.show',$post->id) }}" class="btn btn-primary btn-lg">Rate Post &raquo;</a>
+
+            <a href="{{url('post')}}/{{ $post->user_id }}" class="btn btn-info btn-lg">Edit Post &raquo;</a>
+            <a href="{{url('post')}}/{{ $post->id }}/{{ $post->user_id }}" class="btn btn-danger btn-lg">Delete &raquo;</a>
+
+        </strong></p>
+    <hr />
+
+    @endforeach
+    @endif
+
     @endauth
 </div>
 @endsection
