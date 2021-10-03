64bfe47cf6f84919aee71dea218da34ddf05e4f7
cliff mathebula <climathe1@gmail.com>
2021-10-03 04:44:09 -0700

    feat: added code to return all posts to index to be displayed on the template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests\PostRequest;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
-{
-    
+{   
     public function index() 
     {
-        return view('home.index');
+        $posts = Post::all();
+        return view('home.index', compact('posts'));
     }
+
 }
