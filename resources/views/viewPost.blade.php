03cbefd1e5232518fda875060c78b6b6359a10b8
cliff mathebula <climathe1@gmail.com>
2021-10-03 01:13:55 -0700

    feat: added total reviews

--- resources/views/viewPost.blade.php
+++ resources/views/viewPost.blade.php
@@ -18,77 +18,33 @@
 
                         {{ csrf_field() }}
 
-                    <div class="card">
+                        <div class="card">
 
-                        <div class="container-fliud">
+                            <div class="container-fliud">
 
-                            <div class="wrapper row">
+                                <div class="wrapper row">
 
-                                <div class="preview col-md-6">
+                                    <div class="details col-md-6">
 
-                                    
+                                        <h3 class="product-title">Post Ratings</h3>
 
-                                    <div class="preview-pic tab-content">
+                                        <div class="rating">
 
-                                      <div class="tab-pane active" id="pic-1"><img src="https://dummyimage.com/500x450/000/fff" /></div>
+                                            <input id="input-1" name="rate" class="rating rating-loading" data-min="0" data-max="5" data-step="1" value="{{ $post->userAverageRating }}" data-size="xs">
 
-                                    </div>
+                                            <input type="hidden" name="id" required="" value="{{ $post->id }}">
+                                            @php ($total_reviews = 0 )
+                                            @foreach($post->ratings as $rate)
+                                            @php ($total_reviews =$total_reviews + $rate->rating)
 
+                                            @endforeach
+                                            <span class="review-no"> Total Reviews {{ $total_reviews }}</span>
 
+                                            <br /><br />
 
-                                </div>
-
-                                <div class="details col-md-6">
-
-                                    <h3 class="product-title">Blog Post Ratings</h3>
-
-                                    <div class="rating">
-
-                                        <input id="input-1" name="rate" class="rating rating-loading" data-min="0" data-max="5" data-step="1" value="{{ $post->userAverageRating }}" data-size="xs">
-
-                                        <input type="hidden" name="id" required="" value="{{ $post->id }}">
-
-                                        <span class="review-no">422 reviews</span>
-
-                                        <br/>
-
-                                        <button class="btn btn-success">Submit Review</button>
-
-                                    </div>
-
-                                    <p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
-
-                                    <h4 class="price">current price: <span>$180</span></h4>
-
-                                    <p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
-
-                                    <h5 class="sizes">sizes:
-
-                                        <span class="size" data-toggle="tooltip" title="small">s</span>
-
-                                        <span class="size" data-toggle="tooltip" title="medium">m</span>
-
-                                        <span class="size" data-toggle="tooltip" title="large">l</span>
-
-                                        <span class="size" data-toggle="tooltip" title="xtra large">xl</span>
-
-                                    </h5>
-
-                                    <h5 class="colors">colors:
-
-                                        <span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
-
-                                        <span class="color green"></span>
-
-                                        <span class="color blue"></span>
-
-                                    </h5>
-
-                                    <div class="action">
-
-                                        <button class="add-to-cart btn btn-default" type="button">add to cart</button>
-
-                                        <button class="like btn btn-default" type="button"><span class="fa fa-heart"></span></button>
+                                            <button class="btn btn-lg btn-success">Submit Review &raquo;</button>
+                                            <a class="btn btn-lg btn-primary" href="{{'/posts'}}" role="button">Back to all Posts </a>
+                                        </div>
 
                                     </div>
 
@@ -97,8 +53,6 @@
                             </div>
 
                         </div>
-
-                    </div>
 
                     </form>
 
@@ -117,9 +71,7 @@
 
 
 <script type="text/javascript">
-
     $("#input-id").rating();
-
 </script>
 
 @endsection
