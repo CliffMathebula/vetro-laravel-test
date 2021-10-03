ca7f2b8b55d744cf0860011a215bf6f50efb8fca
cliff mathebula <climathe1@gmail.com>
2021-10-03 01:16:02 -0700

    feat: added destroy method to delete post that belongs to loggedin user

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
@@ -40,6 +42,7 @@ class PostsController extends Controller
     public function show($id)
     {
         $post = Post::find($id);
+
         return view('viewPost', compact('post'));
     }
 
@@ -52,13 +55,71 @@ class PostsController extends Controller
     public function postPost(Request $request)
     {
         request()->validate(['rate' => 'required']);
-        
-        $post = Post::find($request->id); 
+
+        $post = Post::find($request->id);
         $rating = new \willvincent\Rateable\Rating;
         $rating->rating = $request->rate;
         $rating->user_id = auth()->user()->id;
         $post->ratings()->save($rating);
 
         return redirect()->route("posts");
+    }
+
+
+    /**
+     * show Create post template
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show_create()
+    {
+        return view('createPost');
+    }
+
+
+    /**
+     * Create post
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function create_post(Request $request)
+    {
+        $user_id = Auth::id();
+
+        request()->validate([
+            'name' => ['required', 'string', 'min:5', 'max:200'],
+            'title' => ['required', 'string', 'min:5', 'max:200'],
+            'content' => ['required', 'string', 'min:50', 'max:5000']
+        ]);
+
+        if (Post::create([
+            'user_id' => $user_id,
+            'name' => $request['name'],
+            'title' => $request['title'],
+            'content' => $request['content']
+        ])) {
+            return redirect('/posts')->with('success', "Post successfully created.");
+        }
+        return redirect('/post')->with('errors', "Failed to create post.");
+    }
+
+    /**
+     * destroy post
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request) //delete post for current logged in user
+    {
+        $user_id = Auth::id();
+        $author_id = $request['user_id'];
+        $post_id = $request['id'];
+
+        if ($user_id == $author_id) { //checks if post belong to user
+            DB::delete('delete from posts where id = ?', [$post_id]);
+            return ('<script type="text/javascript">alert("Post deleted successfully");window.location.href = "/posts";</script>');
+        }
+        return ('<script type="text/javascript">alert("Failed to Delete Post, Post Belongs to another User"); window.location.href = "/posts"; </script>');
     }
 }
